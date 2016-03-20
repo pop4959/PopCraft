3a0e4bcdc142b17af27d7f0da3c65ff8ec5a7dbb
pop4959 <pop4959@gmail.com>
2016-03-19 23:26:40 -0700

    Player no longer has to be online to get UUID

--- src/main/java/org/popcraft/popcraft/commands/Uuid.java
+++ src/main/java/org/popcraft/popcraft/commands/Uuid.java
@@ -1,28 +1,25 @@
 package org.popcraft.popcraft.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.popcraft.popcraft.utils.MCAPI;
 import org.popcraft.popcraft.utils.Message;
 
 public class Uuid implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	Player player = (Player) sender;
 	if (cmd.getName().equalsIgnoreCase("uuid")) {
 	    if (args.length == 1) {
-		try {
-		    Message.normal(player,
-			    args[0] + "'s UUID: " + ChatColor.RED + Bukkit.getPlayer(args[0]).getUniqueId().toString());
-		} catch (NullPointerException e) {
-		    Message.error(player, "That player does not exist!");
-		}
+		String uuid = MCAPI.getUUID(args[0]);
+		if (uuid != null)
+		    Message.normal(sender, args[0] + "'s UUID: " + ChatColor.RED + uuid);
+		else
+		    Message.error(sender, "Cannot get UUID for that user.");
 	    } else {
-		Message.usage(player, "uuid <username>");
+		Message.usage(sender, "uuid <username>");
 	    }
 	    return true;
 	}
