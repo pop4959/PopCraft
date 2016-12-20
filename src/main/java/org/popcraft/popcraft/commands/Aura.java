e8a7ffccc3e3f9360da607580925c621af08ebe6
pop4959 <pop4959@gmail.com>
2016-12-20 02:34:41 -0800

    Use new Particle API

--- src/main/java/org/popcraft/popcraft/commands/Aura.java
+++ src/main/java/org/popcraft/popcraft/commands/Aura.java
@@ -3,7 +3,7 @@ package org.popcraft.popcraft.commands;
 import java.util.HashMap;
 import java.util.UUID;
 import org.bukkit.ChatColor;
-import org.bukkit.Effect;
+import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,7 +16,7 @@ import org.popcraft.popcraft.utils.Message;
 
 public class Aura implements Listener, CommandExecutor {
 
-    public static HashMap<UUID, Effect> playeraura = new HashMap<UUID, Effect>();
+    public static HashMap<UUID, Particle> playeraura = new HashMap<UUID, Particle>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -42,16 +42,16 @@ public class Aura implements Listener, CommandExecutor {
 		} else if (args[0].equalsIgnoreCase("type")) {
 		    Message.normal(player, "Auras: " + ChatColor.RESET + "clouds, flames, smoke, sparks");
 		} else if (args[0].equals("sparks")) {
-		    playeraura.put(player.getUniqueId(), Effect.FIREWORKS_SPARK);
+		    playeraura.put(player.getUniqueId(), Particle.FIREWORKS_SPARK);
 		    Message.normal(player, "Aura effect set to " + ChatColor.RED + "sparks" + ChatColor.GOLD + ".");
 		} else if (args[0].equals("smoke")) {
-		    playeraura.put(player.getUniqueId(), Effect.LARGE_SMOKE);
+		    playeraura.put(player.getUniqueId(), Particle.SMOKE_LARGE);
 		    Message.normal(player, "Aura effect set to " + ChatColor.RED + "smoke" + ChatColor.GOLD + ".");
 		} else if (args[0].equals("flames")) {
-		    playeraura.put(player.getUniqueId(), Effect.FLAME);
+		    playeraura.put(player.getUniqueId(), Particle.FLAME);
 		    Message.normal(player, "Aura effect set to " + ChatColor.RED + "flames" + ChatColor.GOLD + ".");
 		} else if (args[0].equals("clouds")) {
-		    playeraura.put(player.getUniqueId(), Effect.CLOUD);
+		    playeraura.put(player.getUniqueId(), Particle.CLOUD);
 		    Message.normal(player, "Aura effect set to " + ChatColor.RED + "clouds" + ChatColor.GOLD + ".");
 		} else {
 		    Message.usage(player, "aura <clear/list/type>");
@@ -67,8 +67,9 @@ public class Aura implements Listener, CommandExecutor {
     public void onPlayerMove(PlayerMoveEvent event) {
 	Player player = event.getPlayer();
 	if (playeraura.containsKey(player.getUniqueId())) {
-	    Effect aura = playeraura.get(player.getUniqueId());
-	    player.getWorld().spigot().playEffect(player.getLocation(), aura, 0, 0, 0, 0, 0, 1, 5, 32);
+	    Particle aura = playeraura.get(player.getUniqueId());
+	    //player.getWorld().spigot().playEffect(player.getLocation(), aura, 0, 0, 0, 0, 0, 1, 5, 32);
+	    player.getWorld().spawnParticle(aura, player.getLocation(), 5, 0, 0, 0, 1, null);
 	}
     }
 
