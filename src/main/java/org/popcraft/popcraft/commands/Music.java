e0ae88bb78cd562d8990d5d7d5e7e8dc7f389d8d
pop4959 <pop4959@gmail.com>
2016-12-26 23:37:48 -0800

    Fixed music command

--- src/main/java/org/popcraft/popcraft/commands/Music.java
+++ src/main/java/org/popcraft/popcraft/commands/Music.java
@@ -1,8 +1,11 @@
 package org.popcraft.popcraft.commands;
 
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.ChatColor;
-import org.bukkit.Effect;
-import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,64 +14,40 @@ import org.popcraft.popcraft.utils.Message;
 
 public class Music implements CommandExecutor {
 
+    private static HashMap<UUID, Sound> playing = new HashMap<UUID, Sound>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 	Player player = (Player) sender;
 	if (cmd.getName().equalsIgnoreCase("music")) {
-	    Material disk = Material.GOLD_RECORD;
-	    if (args.length == 0) {
-	    } else {
-		if (args[0].equalsIgnoreCase("13"))
-		    disk = Material.GOLD_RECORD;
-		else if (args[0].equalsIgnoreCase("13"))
-		    disk = Material.GOLD_RECORD;
-		else if (args[0].equalsIgnoreCase("cat"))
-		    disk = Material.GREEN_RECORD;
-		else if (args[0].equalsIgnoreCase("blocks"))
-		    disk = Material.RECORD_3;
-		else if (args[0].equalsIgnoreCase("chirp"))
-		    disk = Material.RECORD_4;
-		else if (args[0].equalsIgnoreCase("far"))
-		    disk = Material.RECORD_5;
-		else if (args[0].equalsIgnoreCase("mall"))
-		    disk = Material.RECORD_6;
-		else if (args[0].equalsIgnoreCase("mellohi"))
-		    disk = Material.RECORD_7;
-		else if (args[0].equalsIgnoreCase("stal"))
-		    disk = Material.RECORD_8;
-		else if (args[0].equalsIgnoreCase("strad"))
-		    disk = Material.RECORD_9;
-		else if (args[0].equalsIgnoreCase("ward"))
-		    disk = Material.RECORD_10;
-		else if (args[0].equalsIgnoreCase("11"))
-		    disk = Material.RECORD_11;
-		else if (args[0].equalsIgnoreCase("wait"))
-		    disk = Material.RECORD_12;
-		else
-		    disk = Material.GOLD_RECORD;
-	    }
-	    if (args.length == 1) {
+	    try {
 		if (args[0].equalsIgnoreCase("list")) {
 		    Message.normal(player, "Discs: " + ChatColor.RESET
-			    + "13, cat, blocks, chirp, far, mall, mellohi, stal, strad, ward, 11, wait");
+			    + "11, 13, blocks, cat, chirp, far, mall, mellohi, stal, strad, wait, ward");
+		} else if (args[0].equalsIgnoreCase("stop")) {
+		    if (playing.containsKey(player.getUniqueId())) {
+			player.stopSound(playing.get(player.getUniqueId()));
+			playing.remove(player.getUniqueId());
+			Message.normal(player, "Stopped music.");
+		    } else
+			Message.error(player, "You are not playing anything right now!");
 		} else {
-		    if (args[0].equalsIgnoreCase("13") || args[0].equalsIgnoreCase("cat")
-			    || args[0].equalsIgnoreCase("blocks") || args[0].equalsIgnoreCase("chirp")
-			    || args[0].equalsIgnoreCase("far") || args[0].equalsIgnoreCase("mall")
-			    || args[0].equalsIgnoreCase("mellohi") || args[0].equalsIgnoreCase("stal")
-			    || args[0].equalsIgnoreCase("strad") || args[0].equalsIgnoreCase("ward")
-			    || args[0].equalsIgnoreCase("11") || args[0].equalsIgnoreCase("wait")) {
-			player.playEffect(player.getLocation(), Effect.RECORD_PLAY, disk);
-			Message.normal(player, "Playing disc: " + ChatColor.RED + args[0].toLowerCase());
-		    } else {
-			Message.error(player, "That music disc does not exist!");
-		    }
+		    Sound sound = Sound.valueOf("RECORD_" + args[0].toUpperCase());
+		    float pitch = 1;
+		    if (args.length >= 2)
+			pitch = Float.parseFloat(args[1]);
+		    if (playing.containsKey(player.getUniqueId()))
+			player.stopSound(playing.get(player.getUniqueId()));
+		    playing.put(player.getUniqueId(), sound);
+		    player.playSound(player.getLocation(), sound, Float.MAX_VALUE, pitch);
+		    Message.normal(player, "Playing disc: " + ChatColor.RED + args[0].toLowerCase() + (pitch == 1 ? ""
+			    : " (" + (new DecimalFormat("#.##")).format(pitch > 2 ? 2 : pitch < 0.5 ? 0.5 : pitch)
+				    + "x speed)"));
 		}
-	    } else {
-		Message.usage(player, "music <disc/list>");
+	    } catch (Exception e) {
+		Message.usage(player, "music <disc/list/stop> [speed]");
 	    }
-	    return true;
 	}
-	return false;
+	return true;
     }
 }
