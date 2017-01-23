952458068a013096c87f4c5ac07211c13b58cd79
pop4959 <pop4959@gmail.com>
2017-01-22 23:59:59 -0800

    Added spoof command

---
+++ src/main/java/org/popcraft/popcraft/commands/Spoof.java
@@ -0,0 +1,33 @@
+package org.popcraft.popcraft.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.popcraft.popcraft.utils.Message;
+
+public class Spoof implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	Player player = (Player) sender;
+	if (cmd.getName().equalsIgnoreCase("spoof")) {
+	    if (args.length == 1) {
+		if (args[0].equalsIgnoreCase("join"))
+		    Bukkit.broadcastMessage(ChatColor.GREEN + "\u2714 " + player.getName());
+		else if (args[0].equalsIgnoreCase("quit"))
+		    Bukkit.broadcastMessage(ChatColor.GREEN + "\u2715 " + player.getName());
+		else if (args[0].equalsIgnoreCase("afk"))
+		    Bukkit.broadcastMessage(ChatColor.GRAY + "* " + player.getDisplayName() + ChatColor.GRAY + " is now AFK.");
+		else if (args[0].equalsIgnoreCase("no-afk"))
+		    Bukkit.broadcastMessage(ChatColor.GRAY + "* " + player.getDisplayName() + ChatColor.GRAY + " is no longer AFK.");
+		else
+		    Message.usage(player, "spoof <join/quit/afk/no-afk>");
+	    } else
+		Message.usage(player, "spoof <join/quit/afk/no-afk>");
+	}
+	return true;
+    }
+}
