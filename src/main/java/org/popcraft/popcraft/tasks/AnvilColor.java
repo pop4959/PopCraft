d74e05f1871f239b0085c69b09d71bfb0ee37f09
pop4959 <pop4959@gmail.com>
2018-12-20 22:51:39 -0800

    port jon's anvil color update

--- src/main/java/org/popcraft/popcraft/tasks/AnvilColor.java
+++ src/main/java/org/popcraft/popcraft/tasks/AnvilColor.java
@@ -1,53 +1,42 @@
 package org.popcraft.popcraft.tasks;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.AnvilInventory;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import static org.bukkit.ChatColor.translateAlternateColorCodes;
 
 public class AnvilColor implements Listener {
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        if (!e.isCancelled()) {
-            Inventory inv = e.getInventory();
-            if (inv instanceof AnvilInventory) {
-                HumanEntity entity = e.getWhoClicked();
-                if (entity instanceof Player) {
-                    Player p = (Player) entity;
-                    if (p.hasPermission("popcraft.anvilcolor")) {
-                        InventoryView view = e.getView();
-                        int rawSlot = e.getRawSlot();
-                        if (rawSlot == view.convertSlot(rawSlot)) {
-                            if (rawSlot == 2) {
-                                ItemStack item = e.getCurrentItem();
-                                if (item != null) {
-                                    ItemMeta meta = item.getItemMeta();
-                                    if (meta != null) {
-                                        if (meta.hasDisplayName()) {
-                                            ItemMeta m = item.getItemMeta();
-                                            String displayName = meta.getDisplayName();
-                                            if (!p.hasPermission("popcraft.anvilcolor.magic")
-                                                    && displayName.contains("&k"))
-                                                displayName.replace("&k", "");
-                                            m.setDisplayName(ChatColor.translateAlternateColorCodes('&', displayName));
-                                            item.setItemMeta(m);
-                                            e.setCurrentItem(item);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+    public void onPrepareAnvilEvent(final PrepareAnvilEvent event) {
+        for (HumanEntity h : event.getViewers()) {
+            if (!h.hasPermission("popcraft.anvilcolor")) {
+                return;
             }
         }
+
+        final String renameText = translateAlternateColorCodes('&', this.getCleanedName(event));
+        final ItemMeta meta = event.getResult().getItemMeta();
+        if (meta != null) {
+            meta.setDisplayName(renameText);
+            event.getResult().setItemMeta(meta);
+        }
+    }
+
+    protected String getCleanedName(final PrepareAnvilEvent event) {
+        final String renamedText = event.getInventory().getRenameText();
+
+        final boolean everyoneHasPermissions = event.getViewers()
+                .stream()
+                .map(entity -> entity.hasPermission("popcraft.anvilcolor.magic"))
+                .reduce(Boolean.FALSE, (left, right) -> left && right);
+
+        if (!everyoneHasPermissions && renamedText != null) {
+            return renamedText.replace("&k", "");
+        }
+        return renamedText == null ? "" : renamedText;
     }
 }
