b50006c453981fee04aba33f26e4a2bc238a94dc
Christopher Bohn <pop4959@gmail.com>
2023-08-13 00:10:24 -0700

    Add new mob types

--- src/main/java/org/popcraft/popcraft/commands/CommandPiggyback.java
+++ src/main/java/org/popcraft/popcraft/commands/CommandPiggyback.java
@@ -40,7 +40,8 @@ public class CommandPiggyback extends PopCraftCommand {
                     PufferFish.class, Rabbit.class, Salmon.class, Sheep.class, SkeletonHorse.class,
                     Snowman.class, Squid.class, TropicalFish.class, Turtle.class, Villager.class,
                     WanderingTrader.class, Bee.class, Dolphin.class, IronGolem.class, Llama.class,
-                    Panda.class, PolarBear.class, Wolf.class, Goat.class);
+                    Panda.class, PolarBear.class, Wolf.class, Goat.class, Axolotl.class, GlowSquid.class,
+                    Allay.class, Frog.class, Tadpole.class, Camel.class, Sniffer.class);
         }
     }
 
